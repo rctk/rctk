39bfa1cb0d9cb775140e81a5c5e05d9c69acaf0b
Ivo van der Wijk <github@in.m3r.nl>
2010-03-23 08:44:16 +0000

    Doh!
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@145 286bb87c-ec97-11de-a004-2f18c49ebcc3

---
+++ rctk/serve_webpy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from rctk.webpy import serve
+
+def main():
+    import sys
+    import os
+
+    args = sys.argv
+    if len(args) != 2:
+        print >> sys.stderr, "Usage: serve_webpy.py module.class"
+        sys.exit(-1)
+    if '.' not in args[1]:
+        print >> sys.stderr, "Usage: serve_webpy.py module.class"
+        sys.exit(-1)
+
+    m, k = args[1].rsplit(".", 1)
+    mod = __import__(m, fromlist=[k])
+    klass = getattr(mod, k)
+
+    ## web.py scans the arguments as well
+    del args[1]
+
+    ## so /static works
+    os.chdir(os.path.dirname(__file__))
+
+    serve(klass)
+
+if __name__ == '__main__':
+    main()
+

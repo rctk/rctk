a28f03e833bbc8d1e4333f39b90f7a1f37a8c47c
Ivo van der Wijk <github@in.m3r.nl>
2010-03-31 13:01:58 +0000

    allow button text to be updated
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@155 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/widgets/button.py
+++ rctk/widgets/button.py
@@ -1,3 +1,4 @@
+import cgi
 from control import Control
 
 from rctk.task import Task
@@ -7,6 +8,18 @@ class Button(Control, Clickable):
     name = "button"
 
     def __init__(self, tk, text, **properties):
-        super(Button, self).__init__(tk, text=text, **properties)
+        self._text = text
+        super(Button, self).__init__(tk, **properties)
 
-    properties = Control.extend(text="")
+    def create(self):
+        self.tk.create_control(self, text=cgi.escape(self.text))
+
+    def _get_text(self):
+        return self._text
+
+    def _set_text(self, text):
+        self._text = text
+        self.tk.queue(Task("Button update id %d test '%s'" % (self.id, self._text), {'control':self.name, 'id':self.id, 'action':"update", "update":{"text":cgi.escape(self.text)}}))
+
+    text = property(_get_text, _set_text)
+

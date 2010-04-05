95a1bf055b13c275a86667b0bfca7ac08e0e7c34
Ivo van der Wijk <github@in.m3r.nl>
2010-04-05 16:39:47 +0000

    Demo class can be specified on cli (like serve_webpy), scan for an available port. Bind on localhost. Some refactoring
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@194 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/serve_mozilla.py
+++ rctk/serve_mozilla.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import cgi
 
 try:
     import gtk
@@ -9,20 +11,26 @@ except ImportError, e:
 
 import BaseHTTPServer
 import rctk
-import os.path
-import cgi
 
 from rctk.toolkit import Toolkit
-from rctkdemos.all import Demo
 
 import gobject
 import simplejson
 
 basedir = os.path.dirname(rctk.__file__)
 
-tk = Toolkit(Demo())
+class PersistentRequestFactoryClass(object):
+    def __init__(self, app):
+        self.tk = Toolkit(app())
+
+    def __call__(self, *args):
+        return StandaloneHTTPServer(self.tk, *args)
 
 class StandaloneHTTPServer(BaseHTTPServer.BaseHTTPRequestHandler):
+    def __init__(self, tk, *args):
+        self.tk = tk
+        BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, *args)
+
     def do_GET(self):
         print "GET", self.path
         if self.path == '/':
@@ -37,13 +45,14 @@ class StandaloneHTTPServer(BaseHTTPServer.BaseHTTPRequestHandler):
         method = self.path[1:]
         length = self.headers.get('content-length', -1)
         q = self.rfile.read(int(length))
-        print "Q", q
         arguments = cgi.parse_qs(q)
+        ## parse_qs always (?) returns lists of values, we need individual 
+        ## values
         for k in arguments:
             v = arguments[k]
             if isinstance(v, list):
                 arguments[k] = v[0]
-        res = tk.handle(method, **arguments)
+        res = self.tk.handle(method, **arguments)
         res = simplejson.dumps(res)
         self.send_response(200, "Ok")
         self.send_header("content-type", "application/json")
@@ -56,25 +65,70 @@ class StandaloneHTTPServer(BaseHTTPServer.BaseHTTPRequestHandler):
         # print "RES", res
 
 
+import socket
 
-## http://majorsilence.com/pygtk_embedded_web_browsers
-class StandaloneRCTK(): 
-    def __init__(self):
-        self.moz = gtkmozembed.MozEmbed()
-                
-        self.server = BaseHTTPServer.HTTPServer(('', 31337), StandaloneHTTPServer)
-        gobject.io_add_watch(self.server.socket, gobject.IO_IN, self.handle_request)
-        win = gtk.Window()
-        win.set_size_request(1200, 800)
-        # win.set_size_request(12, 8)
-        win.add(self.moz)
-        win.show_all()
-        self.moz.load_url('http://localhost:31337')
+class StandaloneServer(object):
+    ## derive from BaseHTTPServer.HTTPServer in stead?
+    def __init__(self, klass):
+        self.port = -1
+        self.klass = klass
+
+    def start(self):
+        factory = PersistentRequestFactoryClass(self.klass)
+        for i in range(31337, 31337 + 50):
+            try:
+                self.server = BaseHTTPServer.HTTPServer(('localhost', i), factory)
+                self.port = i
+                break
+            except socket.error, e:
+                if e.args[0] == 98: ## In use
+                    continue
+                raise
+        if self.port != -1:
+            gobject.io_add_watch(self.server.socket, gobject.IO_IN, self.handle_request)
 
     def handle_request(self, *a):
         self.server.handle_request()
         return True
 
-def main():
-    s = StandaloneRCTK()
+## http://majorsilence.com/pygtk_embedded_web_browsers
+class StandaloneRCTK(object): 
+    """ The actual viewer. Yes, it can be that simple """
+    def __init__(self, appurl):
+        self.moz = gtkmozembed.MozEmbed()
+                
+        win = gtk.Window()
+        win.set_size_request(1200, 800)
+        win.add(self.moz)
+        win.show_all()
+        self.moz.load_url(appurl)
+
+        ## todo: handle close window
+
+def run(klass):
+    server = StandaloneServer(klass)
+    server.start()
+    port = server.port
+    if port == -1:
+        print >> sys.stderr, "Couldn't find an available port"
+        sys.exit(-1)
+    s = StandaloneRCTK("http://localhost:%d/" % port)
     gtk.main()
+
+def main():
+
+    args = sys.argv
+    if len(args) < 2:
+        print >> sys.stderr, "Usage: serve_mozilla.py module.class"
+        sys.exit(-1)
+    if '.' not in args[1]:
+        print >> sys.stderr, "Usage: serve_mozilla.py module.class"
+        sys.exit(-1)
+
+    m, k = args[1].rsplit(".", 1)
+    mod = __import__(m, fromlist=[k])
+    klass = getattr(mod, k)
+
+    run(klass)
+
+

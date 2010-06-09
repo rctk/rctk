3b10efe75af6c37030e9fb95b2248e97ee189842
Arjen Roodselaar <arjenroodselaar@gmail.com>
2010-06-09 13:07:28 +0000

    Fixed dummy control hack, improve operations somewhat. Layout problems still persist.
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@319 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/widgets/collection.py
+++ rctk/widgets/collection.py
@@ -12,29 +12,20 @@ class Collection(Panel):
         self.setLayout(VBox())
         self.widget_class = widget_class
         self._items = []
-        """ Empty panels seem to break the layout manager. Insert
-         a dummy StaticText for now and hide if from self by
-         clearing self._controls. This is a really ugly hack and
-         what we should do is fix the layout manager.
-         FIXME: fix layout manger to support empty panels
-        """ 
-        dummy = StaticText(tk, 'Dummy Control')
-        dummy.visible = False
-        super(Collection, self).append(dummy)
-        self._controls = [] # hide dummy control
         self.extend(items)
     
     def append(self, x):
-        print "calling append"
         w = self.widget_class(self.tk, x)
         self._items.append((x, w))
         super(Collection, self).append(w)
+        self.layout()
 
     def remove(self, x):
         for (a, w) in copy(self._items):
             if x == a:
                 super(Collection, self).remove(w)
                 self._items.remove((x, w))
+        self.layout()
     
     def extend(self, L):
         for x in L:
@@ -44,4 +35,6 @@ class Collection(Panel):
         for w in copy(self._controls):
             super(Collection, self).remove(w)
         self._items = []
+        self.layout()
+    
     

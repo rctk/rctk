70d373588cd76058fff8f38b8aa0d7b155c91bae
Arjen Roodselaar <arjenroodselaar@gmail.com>
2010-08-09 10:02:48 +0000

    Use cookies to track session, try/catch around session.handle, should be implemented properly to handle application errors
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@352 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/webpy.py
+++ rctk/webpy.py
@@ -12,40 +12,51 @@ class WebPyGateway(object):
         self.manager = manager
 
     def GET(self, data):
-        data = data.strip()
-        if data == "":
-            sessionid = self.manager.create()
-            web.seeother('/' + sessionid + '/')
-            return
-
-        sessionid, rest = data.split('/', 1)
-        session = self.manager.get(sessionid)
+        session = self.get_active_session()
+        
         if session is None:
+            sessionid = self.manager.create()
+            web.setcookie('rctk-sid', sessionid)
             web.seeother('/')
             return
 
-        type, data = session.serve(rest)
+        type, result = session.serve(data.strip())
         web.header("content-type", type)
         self.manager.cleanup_expired()
-        return data
-
-
+        return result
+    
     def POST(self, data):
-        data = data.strip()
+        session = self.get_active_session()
 
-        sessionid, rest = data.split('/', 1)
-        session = self.manager.get(sessionid)
         if session is None:
             web.seeother('/')
             return
 
         web.header("content-type", "application/json")
-        method = rest.strip()
+        method = data.strip()
         arguments = web.input()
 
         self.manager.cleanup_expired()
-        result = session.handle(method, **arguments)
+        try:
+            result = session.handle(method, **arguments)
+        except:
+            # TODO: something has gone wrong, find out what and handle session cleanup or recovery.
+            pass
         return simplejson.dumps(result)
+
+    def get_active_session(self):
+        """ Use a cookie to find out if the client already has an active session.
+            Returns the session or None if valid session is found.
+        """
+        sessionid = web.cookies().get('rctk-sid')
+        if sessionid:
+            session = self.manager.get(sessionid)
+            if session is None:
+                # user has a cookie, but no session present so delete the cookie
+                web.setcookie('rctk-sid', sessionid, expires=-84000)
+            else:
+                return session
+        return None    
 
     def __call__(self):
         """

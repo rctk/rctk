f56d99abbbf1db9377e5fd62615a77bb9ac26d18
Ivo van der Wijk <github@in.m3r.nl>
2010-08-16 15:04:56 +0000

    googlecode madness
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@377 286bb87c-ec97-11de-a004-2f18c49ebcc3

---
+++ rctk/widgets/image.py
@@ -0,0 +1,34 @@
+import os, cgi
+from rctk.widgets.control import Control, remote_attribute
+from rctk.resourceregistry import addResource, BaseResource, FileResource
+from rctk.event import Clickable
+from rctk.task import Task
+
+class Image(Control):
+    name = 'image'
+    
+    title = remote_attribute('title', '', lambda self, s: cgi.escape(s))
+    
+    def __init__(self, tk, resource, title='', **properties):
+        self._resource = resource
+        self._title = title
+        super(Image, self).__init__(tk, **properties)
+    
+    def create(self):
+        self.tk.create_control(self, resource=self._resource.name, title=cgi.escape(self.title))
+        
+    def get_resource(self):
+        return self._resource
+    
+    def set_resource(self, resource):
+        self._resource = resource
+        self.tk.queue(Task("%s id %d attr resource update to '%s'" % 
+            (self.name, self.id, self._resource.name),
+            {
+                'control':self.name,
+                'id':self.id,
+                'action':'update',
+                'update':{'resource':self._resource.name}
+            }))
+    resource = property(get_resource, set_resource) 
+

d9fb842c534e76695eadb499d57354822f3239a9
Ivo van der Wijk <github@in.m3r.nl>
2010-08-16 08:14:17 +0000

    missing import, make resource a property
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@374 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/widgets/image.py
+++ rctk/widgets/image.py
@@ -2,6 +2,7 @@ import os, cgi
 from rctk.widgets.control import Control, remote_attribute
 from rctk.resourceregistry import addResource, BaseResource, FileResource
 from rctk.event import Clickable
+from rctk.task import Task
 
 class Image(Control):
     name = 'image'
@@ -29,7 +30,7 @@ class Image(Control):
                 'action':'update',
                 'update':{'resource':self._resource.name}
             }))
-    
+    resource = property(get_resource, set_resource) 
 
 class ImageData(Image):
     def __init__(self, tk, data, name=None, type=None, title='', **properties):

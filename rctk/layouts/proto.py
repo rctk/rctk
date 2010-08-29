afcd8bee7a5d3000acd85285017a6d26a5ea2549
Ivo van der Wijk <github@in.m3r.nl>
2010-08-29 13:40:14 +0000

    more brainstorming
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@411 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/layouts/proto.py
+++ rctk/layouts/proto.py
@@ -9,13 +9,18 @@ top.maxsize(800, 800)
 f = Tkinter.Frame(top)
 f.pack()
 
-msg = Tkinter.Message(f, text="Hello World sdfdsf sdfsda " * 100)
-msg.grid(row=0, column=0)
+msg1 = Tkinter.Message(f, text="Hello World sdfdsf sdfsda ")
+msg1.grid(row=0, column=0)
 msg = Tkinter.Message(f, text="xxx xxx")
 msg.grid(row=0, column=1)
 msg = Tkinter.Message(f, text="xxx xxx")
 msg.grid(row=1, column=0)
-msg = Tkinter.Message(f, text="xxx xxx")
-msg.grid(row=1, column=1)
+
+def foo(*a, **b):
+    msg1.config(text="Hello World sdfdsf sdfsda " * 100)
+    
+b = Tkinter.Button(f, text="Click me", command=foo)
+b.grid(row=1, column=1)
+
 
 root.mainloop()

dc8640e75179c8560ca0cd64e709fefef58d5d2a
Ivo van der Wijk <github@in.m3r.nl>
2011-09-05 13:02:36 +0000

    explicit top/bottom/left/right scroll support
    git-svn-id: https://rctk.googlecode.com/svn/core/trunk@669 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/widgets/panel.py
+++ rctk/widgets/panel.py
@@ -1,4 +1,3 @@
-from rctk.task import Task
 from rctk.widgets.control import Attribute
 from rctk.widgets.container import Container
 
@@ -9,4 +8,13 @@ class Panel(Container):
     name = "panel"
 
     scrolling = Attribute(False, Attribute.BOOLEAN)
+
+    ## valid: top, bottom, left, right. Create-only attribute. XXX
+    scrollto = Attribute("", Attribute.STRING)
+
+    def scrollbottom(self):
+        self.scrollto = "bottom"
+
+    def scrolltop(self):
+        self.scrollto = "top"
 
